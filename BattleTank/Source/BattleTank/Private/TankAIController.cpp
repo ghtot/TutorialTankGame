3c22ce6ef9daaa3694208020d9b44288fc585e4e
Tóth Gábor <tthgbr2017@gmail.com>
2018-09-11 22:52:41 +0200

    Added TankAIController

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	GetControlledTank()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Controlled AI tank name: %s"), *(GetControlledTank()->GetName()));
+}
